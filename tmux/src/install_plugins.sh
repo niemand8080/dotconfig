dce0916a827177df5dbce1fcb8855e7347a4f68e
niemand8080 <benhamster443@gmail.com>
2025-02-09 16:45:36 +0100

    Removed somethings

--- tmux/src/install_plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Wait until the signal "session_ready" is sent
-tmux wait-for session_ready
-
-# Install plugins
-tmux send-keys -t 1 ~/.tmux/plugins/tpm/scripts/install_plugins.sh Enter
