b012e5cd52d71ad94a11e9bc388b86c68666395c
niemand8080 <benhamster443@gmail.com>
2025-02-09 20:42:20 +0100

    Added mini.surround

---
+++ nvim/src/lua/b2/lazy/surround.lua
@@ -0,0 +1,9 @@
+return {
+    'echasnovski/mini.surround',
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("mini.surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+}
