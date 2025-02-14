e60789d9584502f947f6d7a33b5e847eca3f086a
niemand8080 <benhamster443@gmail.com>
2025-02-14 20:14:17 +0100

    Changed rust-analyzer to rust_analyzer

--- nvim/src/lua/b2/lazy/lsp.lua
+++ nvim/src/lua/b2/lazy/lsp.lua
@@ -26,7 +26,7 @@ return {
         -- Mason setup to install and manage LSP servers
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = { "tailwindcss", "rust-analyzer", "eslint", "html", "cssls", "jsonls", "ts_ls", "lua_ls", "bashls" }, -- Add more as needed
+            ensure_installed = { "tailwindcss", "rust_analyzer", "eslint", "html", "cssls", "jsonls", "ts_ls", "lua_ls", "bashls" }, -- Add more as needed
             automatic_installation = true,                                                                                           -- Automatically install if not present
             handlers = {
                 function(server_name)                                                                                                -- Default handler for most servers
