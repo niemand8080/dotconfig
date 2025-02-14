1848ef460041bd2ef019c173aad767cd994f7c8a
niemand8080 <benhamster443@gmail.com>
2025-02-14 21:16:43 +0100

    Updated the LSP config

--- nvim/src/lua/b2/lazy/lsp.lua
+++ nvim/src/lua/b2/lazy/lsp.lua
@@ -29,7 +29,7 @@ return {
             ensure_installed = { "tailwindcss", "rust_analyzer", "eslint", "html", "cssls", "jsonls", "ts_ls", "lua_ls", "bashls" }, -- Add more as needed
             automatic_installation = true,                                                                                           -- Automatically install if not present
             handlers = {
-                function(server_name) -- Default handler for servers
+                function(server_name)                                                                                                -- Default handler for servers
                     if server_name ~= "rust_analyzer" then
                         require("lspconfig")[server_name].setup({
                             capabilities = lspconfig_defaults.capabilities
@@ -70,20 +70,28 @@ return {
             desc = 'LSP actions',
             callback = function(event)
                 local opts = { buffer = event.buf }
+                local file_type = vim.bo.filetype
 
-                vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+                if file_type == "rust" then
+                    vim.keymap.set('n', 'K', function() vim.cmd.RustLsp({ 'hover', 'actions' }) end, opts)
+                    vim.keymap.set('n', '<leader>a', function() vim.cmd.RustLsp('codeAction') end, opts)
+                else
+                    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+                    vim.keymap.set('n', '<leader>a', function() vim.lsp.buf.code_action() end, opts)
+                end
+
                 vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
                 vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
                 vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
                 vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
                 vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
-                vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
                 vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format() end, opts)
-
                 vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
                 vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+
+                -- insert mode
+                vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
             end,
         })
 
@@ -145,5 +153,10 @@ return {
                 end, { "i", "s", "c" }),
             }
         })
+
+        -- diagnostics style
+        vim.diagnostic.config {
+            float = { border = "rounded" },
+        }
     end
 }
